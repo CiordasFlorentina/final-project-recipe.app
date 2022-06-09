0ef431d1e9297d625ecdca51d58014797d3cc87b
Radus1k <44340929+Radus1k@users.noreply.github.com>
2022-06-09 15:12:06 +0300

    CircuitBreakerBasicsConfigured

--- src/main/java/com/example/recipe/app/controller/IngredientController.java
+++ src/main/java/com/example/recipe/app/controller/IngredientController.java
@@ -6,11 +6,15 @@ import com.example.recipe.app.service.IngredientService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.Response;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -22,6 +26,21 @@ public class IngredientController {
 
     private final IngredientService ingredientService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public void fakeEndpointApiCall(){
+        // Fake api call->throws exception->CircuitBreaker calls fallback function.
+        // For validating the CircuitBreaker functionality
+        restTemplate.getForObject("http://localhost:8081/ingredient", String.class);
+
+    }
+
     private static final String INGREDIENT_SERVICE = "IngredientService";
 
     public IngredientController(IngredientService ingredientService) {
@@ -29,31 +48,38 @@ public class IngredientController {
     }
 
     @GetMapping()
+    @CircuitBreaker(name=INGREDIENT_SERVICE, fallbackMethod="ingredientFallback")
     @ApiOperation(value = "GetAll", notes = "Get all ingredients")
-    public List<Ingredient> getIngredients() {
-        return ingredientService.getIngredients();
+    public ResponseEntity<List<Ingredient>> getIngredients() {
+//        fakeEndpointApiCall();
+        List<Ingredient> listOfIngredients =  ingredientService.getIngredients();
+        return new ResponseEntity<List<Ingredient>>(listOfIngredients, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     @CircuitBreaker(name=INGREDIENT_SERVICE, fallbackMethod="ingredientFallback")
     @ApiOperation(value = "Get", notes = "Get specific ingredient by id")
-    public Ingredient getIngredient(@PathVariable Long id) throws Exception {
-        if(true) {
-            throw new Exception("Exception message"); // Checking CircuitBreaker..
-        }
-        return ingredientService.getIngredient(id);
+    public ResponseEntity<String> getIngredient(@PathVariable Long id) {
+//        fakeEndpointApiCall();
+        String ingredient_to_string = ingredientService.getIngredient(id).toString();
+        return new ResponseEntity<String>(ingredient_to_string, HttpStatus.OK);
     }
-
-    public String ingredientFallback(Exception e){
-        return new String("Server is down");
-    }
-
 
     @PostMapping()
+    @CircuitBreaker(name=INGREDIENT_SERVICE, fallbackMethod="ingredientFallback")
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Add", notes = "Add new ingredient")
-    public Ingredient addIngredient(@Valid @RequestBody IngredientRequest ingredient) {
-        return ingredientService.addIngredient(ingredient.getName());
+    public ResponseEntity<Ingredient> addIngredient(@Valid @RequestBody IngredientRequest ingredient) {
+//        fakeEndpointApiCall();
+        Ingredient _ingredient = ingredientService.addIngredient(ingredient.getName());
+        return new ResponseEntity<>(_ingredient, HttpStatus.OK);
+    }
+
+    // Function called automatically when some errors occurred within Controller Api Calls
+    // Must have the same return data type as the originally called functions
+    public ResponseEntity<String> ingredientFallback(Exception e){
+        return new ResponseEntity<String>("Ingredient Service is down!", HttpStatus.INTERNAL_SERVER_ERROR);
+
     }
 
 }
--- src/main/java/com/example/recipe/app/controller/RecipeController.java
+++ src/main/java/com/example/recipe/app/controller/RecipeController.java
@@ -1,14 +1,20 @@
 package com.example.recipe.app.controller;
 
+import com.example.recipe.app.model.entity.Ingredient;
 import com.example.recipe.app.model.request.IngredientWithQuantity;
 import com.example.recipe.app.model.request.RecipeRequest;
 import com.example.recipe.app.model.response.FullRecipeResponse;
+import com.example.recipe.app.service.IngredientService;
 import com.example.recipe.app.service.RecipeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -20,52 +26,84 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
+    private static final String RECIPE_SERVICE = "RecipeService";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void fakeEndpointApiCall(){
+        // Fake api call->throws exception->CircuitBreaker calls fallback function.
+        // For validating the CircuitBreaker functionality
+        restTemplate.getForObject("http://localhost:8081/recipe", String.class);
+    }
+
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
     @GetMapping()
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ApiOperation(value = "GetAll", notes = "Get all recipes")
-    public List<FullRecipeResponse> getRecipes() {
-        return recipeService.getRecipes();
+    public ResponseEntity<List<FullRecipeResponse>> getRecipes() {
+//         fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.getRecipes(), HttpStatus.OK);
+
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ApiOperation(value = "Get", notes = "Get specific recipe by id")
-    public FullRecipeResponse getRecipe(@PathVariable Long id) {
-        return recipeService.getRecipe(id);
+    public ResponseEntity<FullRecipeResponse> getRecipe(@PathVariable Long id) {
+//         fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.getRecipe(id), HttpStatus.OK);
     }
 
     @PostMapping()
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ApiOperation(value = "Add", notes = "Add new recipe")
     @ResponseStatus(HttpStatus.CREATED)
-    public FullRecipeResponse addRecipe(@Valid @RequestBody RecipeRequest recipe) {
-        return recipeService.addRecipe(recipe);
+    public ResponseEntity<FullRecipeResponse> addRecipe(@Valid @RequestBody RecipeRequest recipe) {
+        // fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.addRecipe(recipe), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ApiOperation(value = "Update", notes = "Update recipe details and ingredient list")
-    public FullRecipeResponse updateRecipe(@PathVariable Long id, @Valid @RequestBody RecipeRequest recipe) {
-        return recipeService.updateRecipe(id, recipe);
+    public ResponseEntity<FullRecipeResponse> updateRecipe(@PathVariable Long id, @Valid @RequestBody RecipeRequest recipe) {
+        // fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.updateRecipe(id, recipe), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation(value = "Delete", notes = "Delete recipe")
     public ResponseEntity<Long> deleteRecipe(@PathVariable Long id) {
+        // fakeEndpointApiCall();
         recipeService.deleteRecipe(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PostMapping("/{id}/addIngredient")
+    @CircuitBreaker(name=RECIPE_SERVICE, fallbackMethod="recipeFallBack")
     @ApiOperation(value = "AddIngredient", notes = "Add new ingredient with quantity to the recipe")
-    public FullRecipeResponse addIngredient(@PathVariable Long id, @Valid @RequestBody IngredientWithQuantity ingredientWithQuantity) {
-        return recipeService.addIngredientToRecipe(id, ingredientWithQuantity);
+    public ResponseEntity<FullRecipeResponse> addIngredient(@PathVariable Long id, @Valid @RequestBody IngredientWithQuantity ingredientWithQuantity) {
+        // fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.addIngredientToRecipe(id, ingredientWithQuantity), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}/removeIngredient/{ingredientId}")
     @ApiOperation(value = "RemoveIngredient", notes = "Remove ingredient from recipe")
-    public FullRecipeResponse removeIngredient(@PathVariable Long id, @PathVariable Long ingredientId) {
-        return recipeService.removeIngredient(id, ingredientId);
+    public ResponseEntity<FullRecipeResponse> removeIngredient(@PathVariable Long id, @PathVariable Long ingredientId) {
+        // fakeEndpointApiCall();
+        return new ResponseEntity<>(recipeService.removeIngredient(id, ingredientId), HttpStatus.OK);
     }
+
+
+    public ResponseEntity<String> recipeFallBack(Exception e){
+        return new ResponseEntity<String>("Recipe Service is down!", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
 }
