64214c52c534d654b3c8c46a7a0cca2444d38838
Flori <ciordas.florentina@gmail.com>
2022-06-21 00:09:26 +0300

    move database in cloud

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,21 @@
-INSERT INTO public.recipe (recipe_id, name) VALUES (101, 'pudding recipe');
-INSERT INTO public.recipe (recipe_id, name) VALUES (201, 'Cereal with milk');
-INSERT INTO public.recipe (recipe_id, name) VALUES (301, 'Fry chicken');
+INSERT INTO public.recipe (recipe_id, name) VALUES (101, 'Vanilla pudding');
+INSERT INTO public.recipe (recipe_id, name) VALUES (102, 'Cereal with milk');
+INSERT INTO public.recipe (recipe_id, name) VALUES (103, 'Fry chicken');
+INSERT INTO public.recipe (recipe_id, name) VALUES (104, 'Hot chocolate');
 
 INSERT INTO public.ingredient (ingredient_id, name) VALUES (101, 'milk');
-INSERT INTO public.ingredient (ingredient_id, name) VALUES (201, 'vanilla extract');
-INSERT INTO public.ingredient (ingredient_id, name) VALUES (301, 'Cereal');
-INSERT INTO public.ingredient (ingredient_id, name) VALUES (401, 'oil');
-INSERT INTO public.ingredient (ingredient_id, name) VALUES (501, 'chicken');
+INSERT INTO public.ingredient (ingredient_id, name) VALUES (102, 'vanilla extract');
+INSERT INTO public.ingredient (ingredient_id, name) VALUES (103, 'Cereal');
+INSERT INTO public.ingredient (ingredient_id, name) VALUES (104, 'oil');
+INSERT INTO public.ingredient (ingredient_id, name) VALUES (105, 'chicken');
+INSERT INTO public.ingredient (ingredient_id, name) VALUES (106, 'cacao');
 
 
 INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (101, '1L', 101, 101);
-INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (201, null, 201, 101);
-INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (301, '0.3L', 101, 201);
-INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (401, '200g', 301, 201);
-INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (501, '50mL', 401, 301);
-INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (601, '500g', 501, 301);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (102, null, 102, 101);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (103, '200mL', 101, 102);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (104, '100g', 103, 102);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (105, '25mL', 104, 103);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (106, '500g', 105, 103);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (107, '200ml', 101, 104);
+INSERT INTO public.recipe_ingredient (recipe_ingredient_id, quantity, ingredient_id, recipe_id) VALUES (108, '40g', 106, 104);
